846b8d4d736de2eb7f87cb770501e588766ec812
chengzixqq <3474046521@qq.com>
2022-05-19 06:26:06 +0800

    Initial commit

---
+++ 1.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main()
+/*{
+    printf("qqq\n");
+    printf("abc");
+    return 0;
+}*/
+{
+    char  ch = 'a';
+    int age = 20;
+    printf("%d\n", sizeof(char));
+
+    return 0;
+}
