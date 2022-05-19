adf9671b3322f3719eb0f443cc350a4a6fb1ee3e
chengzixqq <3474046521@qq.com>
2022-05-19 17:45:46 +0800

    Initial commit

--- 1.c
+++ 1.c
@@ -1,3 +1,5 @@
+#pragma clang diagnostic push
+#pragma ide diagnostic ignored "cert-err34-c"
 #include <stdio.h>
 
 int main()
@@ -6,10 +8,22 @@ int main()
     printf("abc");
     return 0;
 }*/
-{
+
+/*{
     char  ch = 'a';
     int age = 20;
     printf("%d\n", ch);
 
     return 0;
+}*/
+
+{
+    int a = 1;
+    int b = 3;
+    scanf("%d %d",&a,&b);
+    int sum = a + b;
+    printf("sum = %d\n",sum);
+
+    return 0;
 }
+#pragma clang diagnostic pop
